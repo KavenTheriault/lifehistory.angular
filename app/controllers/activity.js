e8eba9459548ca938581963afe453604c96c5078
ZoidQC <zoid@leetzone.ca>
2016-09-17 08:59:02 -0400

    angular bootstrat exemple working

--- app/controllers/activity.js
+++ app/controllers/activity.js
@@ -81,4 +81,21 @@ angular.module('myApp.activities',['ngRoute'])
         return d.promise;
     }
 
+})
+
+.controller('TypeaheadCtrl', function($scope, $http) {
+
+  $scope.getLocation = function(val) {
+    return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
+      params: {
+        address: val,
+        sensor: false
+      }
+    }).then(function(response){
+      return response.data.results.map(function(item){
+        return item.formatted_address;
+      });
+    });
+  };
+  
 });
